d9d0eb3c317e6f9c3956a20cd04842e136e29717
MiR Lee <mirlee@MiRui-MacBookAir.local>
2025-05-14 15:35:14 +0900

    ffff

---
+++ sensor_ver_6.ino
@@ -0,0 +1,126 @@
+#include <SDISerial.h>
+ 
+#define INPUT_SIZE 30
+#define NUMSAMPLES 5
+#define INVERTED 1
+
+// 실제 측정값 기준 (공기와 물에서의 값)
+#define AIR_READING 7.34    // 공기 중에서 나오는 값
+#define WATER_READING 60.0  // 물 속에서 나오는 값 (추정)
+ 
+int sensorDelay = 100; 
+float bulkDens = 0.4;  
+float theta = 1 - (bulkDens / 2.65); 
+char* samples; 
+ 
+SDISerial sdi_serial_connection(6, INVERTED);
+
+void setup() {
+  pinMode(2, OUTPUT);
+  sdi_serial_connection.begin();
+  Serial.begin(9600); 
+  
+  Serial.println("=== TEROS 11 Calibrated ===");
+  Serial.print("Air reading: ");
+  Serial.println(AIR_READING);
+  Serial.print("Water reading: ");
+  Serial.println(WATER_READING);
+  Serial.println("Output: 0-70");
+  Serial.println("=========================");
+}
+
+// 센서값을 0-70으로 매핑
+float mapToScale(float reading) {
+  // 7.34-60 범위를 0-70으로 매핑
+  float mapped = (reading - AIR_READING) / (WATER_READING - AIR_READING) * 70.0;
+  
+  // 범위 제한
+  if (mapped < 0) mapped = 0;
+  if (mapped > 70) mapped = 70;
+  
+  return mapped;
+}
+ 
+void loop() {
+  uint8_t i;
+  float dielectric[NUMSAMPLES];
+  float soilMoist[NUMSAMPLES];
+  float degC[NUMSAMPLES];
+  float bulkEC[NUMSAMPLES];
+  float solutionEC[NUMSAMPLES];
+   
+  float dielMean = 0.0;
+  float soilMoistMean = 0.0;
+  float degCMean = 0.0;
+  float bulkECMean = 0.0;
+  float solutionECMean = 0.0;
+ 
+  // 샘플 수집
+  for (i = 0; i < NUMSAMPLES; i++) {
+    samples = get_measurement();
+    while (strlen(samples) < 5) {
+      samples = get_measurement();  
+    }
+  
+    char* term1 = strtok(samples, "+");
+    
+    term1 = strtok(NULL, "+");
+    dielectric[i] = atof(term1);
+               
+    term1 = strtok(NULL, "+");
+    degC[i] = atof(term1);
+    term1 = strtok(NULL, "+");
+    bulkEC[i] = atof(term1);
+   
+    if (bulkEC[i] < 5) {
+      soilMoist[i] = (5.89 * pow(10.0, -6.0) * pow(dielectric[i], 3.0)) - 
+                     (7.62 * pow(10.0, -4.0) * pow(dielectric[i], 2.0)) + 
+                     (3.67 * pow(10.0, -2.0) * dielectric[i]) - 
+                     (7.53 * pow(10.0, -2.0));
+    } else {
+      soilMoist[i] = 0.118 * sqrt(dielectric[i]) - 0.117;
+    }
+    
+    solutionEC[i] = (bulkEC[i] / 1000 * soilMoist[i]) / theta; 
+ 
+    dielMean += dielectric[i];
+    soilMoistMean += soilMoist[i];
+    degCMean += degC[i];
+    bulkECMean += bulkEC[i];
+    solutionECMean += solutionEC[i];
+  }
+   
+  // 원본 코드의 평균 계산 방식 유지
+  soilMoistMean /= (NUMSAMPLES * 2000);
+  degCMean /= NUMSAMPLES;
+
+  // 7.34-60 범위를 0-70으로 매핑
+  float scaledValue = mapToScale(soilMoistMean);
+
+  // 상태 결정
+  char* status;
+  if (scaledValue < 21) {      // 30% of 70
+    status = "low";
+  }
+  else if (scaledValue < 49) {  // 70% of 70
+    status = "good";
+  }
+  else {
+    status = "high";
+  }
+
+  // 출력
+  Serial.print(scaledValue, 1);
+  Serial.print(", ");
+  Serial.print(degCMean, 1);
+  Serial.print(", ");
+  Serial.println(status);
+
+  delay(sensorDelay);
+}
+
+char* get_measurement(){
+  char* service_request = sdi_serial_connection.sdi_query("?M!", sensorDelay);
+  char* service_request_complete = sdi_serial_connection.wait_for_response(sensorDelay);
+  return sdi_serial_connection.sdi_query("?D0!", sensorDelay);
+}
